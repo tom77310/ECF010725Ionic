23fcac2a68031dddf0c82218f93dbb5594c09e93
Lemaire Thomas <lemairetomtom@gmail.com>
2025-07-01 15:25:10 +0200

    connexion a l'api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,44 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
 
+
+interface Scrutin {
+  title : string;
+  start_at : Date;
+  ends_at : Date;
+}
 
 const Home: React.FC = () => {
 
+const [scrutin, setScrutin] = useState<Scrutin>();
+const [Loading, setLoading] = useState(true);
 
 
+useEffect(()=>{
+  fetch('http://localhost:3000/api/v1/scrutins') // connexion a l'api pour recuperer les scrutins
+  .then(res => res.json())
+  .then(response => {
+      setScrutin(response.data) // recupere les données des scrutins
+      console.log(response.data);
+      
+    })
+    .catch(error => {
+      console.error('Erreur API :', error);
+      setLoading(false);
+    });
+}, [])
 
-
-  
-  return (
+return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Vote App V2</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
         </IonHeader>
+        <h1>{scrutin?.title}</h1>
       </IonContent>
     </IonPage>
   );
